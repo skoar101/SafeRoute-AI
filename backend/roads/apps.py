690c7f7b0acdd961cb94254948c677cd3a3458fd
skoar101 <prerna010908@gmail.com>
2025-10-23 11:39:22 +0530

    Add Django app configuration for 'roads'

---
+++ backend/roads/apps.py
@@ -0,0 +1,8 @@
+"""
+Purpose: Django application configuration for 'roads'.
+"""
+from django.apps import AppConfig
+
+class RoadsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'roads'
