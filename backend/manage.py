cc050f0b0ad589196d9e0c258815517d5e2a859f
skoar101 <prerna010908@gmail.com>
2025-10-23 12:20:43 +0530

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -3,16 +3,20 @@ Purpose: Django's CLI utility for management tasks (migrations, server, etc.)
 """
 import os
 import sys
+print("Starting manage.py")   # <-- add this
+
+import traceback               # <-- add this
+try:
+    from django.core.management import execute_from_command_line
+except Exception as e:
+    print("Error importing Django:", e)
+    traceback.print_exc()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saferouteai.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django."
-        ) from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
+
+
