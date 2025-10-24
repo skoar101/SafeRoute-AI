f74534954cdda608e0e0cbdda6f4e64d06290699
skoar101 <prerna010908@gmail.com>
2025-10-24 13:55:06 +0530

    Resolve merge conflicts properly

--- backend/roads/__init__.py
+++ backend/roads/__init__.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 """
 Purpose: Marks 'roads' as a Python package (required by Django).
 """
-=======
-"""
-Purpose: Marks 'roads' as a Python package (required by Django).
-"""
->>>>>>> 2f55220dae0bb14c7c90517c68fd70ac27d114b2
--- backend/roads/apps.py
+++ backend/roads/apps.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 Purpose: Django application configuration for 'roads'.
 """
@@ -7,13 +7,3 @@ from django.apps import AppConfig
 class RoadsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'roads'
-=======
-"""
-Purpose: Django application configuration for 'roads'.
-"""
-from django.apps import AppConfig
-
-class RoadsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'roads'
->>>>>>> 2f55220dae0bb14c7c90517c68fd70ac27d114b2
