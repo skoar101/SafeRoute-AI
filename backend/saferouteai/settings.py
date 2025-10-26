0dccbd177da584247e0b34da61ddc146b0a38b08
skoar101 <prerna010908@gmail.com>
2025-10-26 17:42:09 +0530

    Add static context processor to fix Render static error

--- backend/saferouteai/settings.py
+++ backend/saferouteai/settings.py
@@ -29,16 +29,18 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR.parent / "frontend" / "templates"],  # parent goes up from backend
         "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
 ]
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
